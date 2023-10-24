805496713024760b004f991b448e1aeb5b820028
sapanasenko <apanas91@gmail.com>
2023-10-25 00:48:09 +0300

    Подготовка тестов к ревью

--- test_product_page.py
+++ test_product_page.py
@@ -10,7 +10,7 @@ from .pages.locators import BasketPageLocators, ProductPageLocators
 
 from selenium import webdriver
 
-
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
@@ -29,7 +29,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -48,7 +48,7 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page.is_not_element_present(*BasketPageLocators.BASKET_FORM)
     basket_page.should_be_message("Your basket is empty")
 
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -78,7 +78,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE)
 
-
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
         # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
--- test_product_page_lesson_4_3_6.py
+++ test_product_page_lesson_4_3_6.py
@@ -3,7 +3,7 @@ import pytest
 
 from .pages.locators import ProductPageLocators
 from .pages.product_page import ProductPage
-
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link, 0)
@@ -17,13 +17,13 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     # page.should_be_product_added(product_name)
     # page.should_be_basket_amount(amount)
 
-
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link, 0)
     page.open()
     assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE)
 
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link, 0)
